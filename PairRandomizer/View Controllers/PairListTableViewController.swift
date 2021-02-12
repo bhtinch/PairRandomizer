5efdbf9196d0966a34b7472abab8dfe971147e1a
Benjamin Tincher <b.h.tincher@gmail.com>
2021-02-12 14:58:11 -0500

    adds ability to update groups based on desired member number

--- PairRandomizer/View Controllers/PairListTableViewController.swift
+++ PairRandomizer/View Controllers/PairListTableViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 class PairListTableViewController: UITableViewController {
     //  MARK: - Outlets
     @IBOutlet weak var randomizeButton: UIButton!
+    @IBOutlet weak var changeGroupNumbersButton: UIButton!
     
     //  MARK: - Lifecyle
     override func viewDidLoad() {
         super.viewDidLoad()
         PersonController.shared.loadFromPersistenceStore()
-        randomize()
+        presentGroupNumberAlert()
         
-        randomizeButton.layer.cornerRadius = 8
-        randomizeButton.layer.masksToBounds = true
+        configureButtons()
     }
     
     //  MARK: - Actions
     @IBAction func addButtonTapped(_ sender: Any) {
         presentAddPersonAlert()
+    }
+    
+    @IBAction func changeGroupNumbersButtonTapped(_ sender: Any) {
+        presentGroupNumberAlert()
     }
     
     @IBAction func randomizeButtonTapped(_ sender: Any) {
@@ -33,10 +37,32 @@ class PairListTableViewController: UITableViewController {
     
     //  MARK: - Properties
     var shuffled: [Person] = []
+    var groupNumber: Int = 2
       
     //  MARK: - Methods
     func randomize(){
         shuffled = PersonController.shared.persons.shuffled()
+    }
+    
+    func presentGroupNumberAlert() {
+        let alertController = UIAlertController(title: "How many members per group?", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter number of members..."
+            textField.keyboardType = .numberPad
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+            guard let valueText = alertController.textFields?.first?.text, !valueText.isEmpty,
+                  let value = Int(valueText) else { return }
+            self.groupNumber = value
+            self.randomize()
+            self.tableView.reloadData()
+        }
+        
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     func presentAddPersonAlert(){
@@ -67,19 +93,19 @@ class PairListTableViewController: UITableViewController {
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        if shuffled.count % 2 == 0 {
-            return shuffled.count / 2
+        if shuffled.count % groupNumber == 0 {
+            return shuffled.count / groupNumber
         } else {
-            return shuffled.count / 2 + 1
+            return shuffled.count / groupNumber + 1
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        if (shuffled.count % 2) == 1 && section == shuffled.count / 2 {
-            return 1
+        if (shuffled.count % groupNumber) != 0 && section == (shuffled.count / groupNumber) {
+            return shuffled.count % groupNumber
         } else {
-            return 2
+            return groupNumber
         }
     }
     
@@ -90,7 +116,7 @@ class PairListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
         
-        let index = indexPath.section * 2 + indexPath.row
+        let index = indexPath.section * groupNumber + indexPath.row
         cell.textLabel?.text = shuffled[index].name
         
         return cell
@@ -99,11 +125,22 @@ class PairListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
-            let index = indexPath.section * 2 + indexPath.row
+            let index = indexPath.section * groupNumber + indexPath.row
             
             PersonController.shared.delete(person: shuffled[index])
             randomize()
             tableView.reloadData()
+        }
+    }
+    
+    //  MARK: - Styling Functions
+    func configureButtons() {
+        let buttons = [randomizeButton, changeGroupNumbersButton]
+        
+        for button in buttons {
+            guard let button = button else { return }
+            button.layer.cornerRadius = 8
+            button.layer.masksToBounds = true
         }
     }
 }
